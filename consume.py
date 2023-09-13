0a1107c23509bf54f42ce2416d011ddc66f754d9
Richard Dalton <richard@devjoy.com>
2023-09-13 21:14:47 +0100

    Change consumer to latest

--- consume.py
+++ consume.py
@@ -4,10 +4,10 @@ import json
 consumer = KafkaConsumer(
     bootstrap_servers='localhost:29092',
     value_deserializer=lambda v: json.loads(v.decode('ascii')),
-    auto_offset_reset='earliest'
+    auto_offset_reset='latest'
 )
 
-consumer.subscribe(topics='testnum')
+consumer.subscribe(topics='testnum', )
 for message in consumer:
     print ("%d:%d: v=%s" % (message.partition,
                             message.offset,
